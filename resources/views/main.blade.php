53295b76eab54bac97db84ab0d7349e1601ebdfc
kylepearson97 <kylepearson124@gmail.com>
2017-07-19 18:06:56 +0100

    stuff
    removed un-needed folders and views, got login system working along with logout

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,6 +6,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Humber Holiday Homes</title>
 
@@ -64,9 +65,33 @@
                     </li>
                     <li>
                         <a href="/contact">Contact</a>
-                    </li>  
+                    </li>
+                    @if (Auth::guest())
+                        <li><a href="{{ route('login') }}">Login</a></li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                <li>
+                                    <a href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();">
+                                        Logout
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
+                    @endif
                 </ul>
             </div>
+            
         </div>
     </nav>
     
