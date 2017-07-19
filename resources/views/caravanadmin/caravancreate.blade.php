fd541942631689041b2420f34fe54ba3f843e3c1
Sam <samuelmercer@googlemail.com>
2017-07-19 22:49:54 +0100

    got admin/caravan to display

--- resources/views/caravanadmin/caravancreate.blade.php
+++ resources/views/caravanadmin/caravancreate.blade.php
@@ -7,7 +7,7 @@
         <div class="col-md-12 col-sm-12 col-xs-12">
             <div class="x_panel">
                 <div class="x_title">
-                    <h2>Insert Caravan <a href="{{route('caravanadmin.caravancreate')}}" class="btn btn-info btn-xs"><i class="fa fa-chevron-left"></i> Back </a></h2>
+                    <h2>Insert Caravan <a href="{{route('caravan.create')}}" class="btn btn-info btn-xs"><i class="fa fa-chevron-left"></i> Back </a></h2>
                     <div class="clearfix"></div>
                 </div>
                 <div class="x_content">
